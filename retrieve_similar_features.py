e1acbbfdf0c24ba29195c69fab064619b3f3439a
Wok <woctezuma@users.noreply.github.com>
2019-02-05 18:44:53 +0100

    Fix array indices for search of most similar Steam banners with OpenCV

--- retrieve_similar_features.py
+++ retrieve_similar_features.py
@@ -1,13 +1,16 @@
 from collections import Counter
+from time import time
 
 import cv2 as cv
 import numpy as np
 
 from build_feature_index import get_descriptor_database_filename, get_descriptor_img_id_filename
-from build_search_index import app_id_to_image_filename
+from build_search_index import app_id_to_image_filename, list_app_ids
+from download_steam_banners import get_app_details
+from retrieve_similar_banners import get_store_url
 
 
-def retrieve_similar_features(query_app_id, min_match_count=10):
+def retrieve_similar_features(query_app_id, descriptor_database=None, descriptor_img_id=None):
     image_filename = app_id_to_image_filename(query_app_id)
     query_img = cv.imread(image_filename, cv.IMREAD_COLOR)
 
@@ -27,10 +30,22 @@ def retrieve_similar_features(query_app_id, min_match_count=10):
 
     flann = cv.FlannBasedMatcher(index_params, search_params)
 
-    descriptor_database = np.load(get_descriptor_database_filename())
-    descriptor_img_id = np.load(get_descriptor_img_id_filename())
+    if descriptor_database is None:
+        descriptor_database = np.load(get_descriptor_database_filename())
 
-    matches = flann.knnMatch(query_des, descriptor_database, k=2)
+    if descriptor_img_id is None:
+        descriptor_img_id = np.load(get_descriptor_img_id_filename())
+
+    app_ids = list_app_ids()
+
+    # Remove the features of the query from the database
+    ind, _ = np.where(descriptor_img_id == app_ids.index(query_app_id))
+    trimmed_descriptor_database = descriptor_database.copy()
+    trimmed_descriptor_database[ind] = 0
+
+    start = time()
+    matches = flann.knnMatch(query_des, trimmed_descriptor_database, k=2)
+    print('Elapsed time: {:.2f} s'.format(time() - start))
 
     # store all the good matches as per Lowe's ratio test.
     good_matches = []
@@ -41,12 +56,36 @@ def retrieve_similar_features(query_app_id, min_match_count=10):
         except IndexError:
             pass
 
-    good_img_ids = [good_match.trainIdx for good_match in good_matches]
+    good_img_ids = [int(descriptor_img_id[good_match.trainIdx]) for good_match in good_matches]
 
-    reference_img_id_counter = Counter(good_img_ids)
+    good_app_ids = [app_ids[img_id] for img_id in good_img_ids]
 
-    return reference_img_id_counter
+    reference_app_id_counter = Counter(good_app_ids)
+
+    return reference_app_id_counter
+
+
+def print_ranking(query_app_id, reference_app_id_counter, num_elements_displayed=10):
+    app_details = get_app_details(query_app_id)
+    app_name = app_details['name']
+    print('\nQuery appID: {} ({} @ {})'.format(query_app_id, app_name, get_store_url(query_app_id)))
+
+    for rank, app_id in enumerate(reference_app_id_counter):
+        app_details = get_app_details(app_id)
+        app_name = app_details['name']
+        print('{}) app: {} ({} @ {})'.format(rank + 1, app_id, app_name, get_store_url(app_id)))
+
+        if rank >= (num_elements_displayed - 1):
+            break
+    return
 
 
 if __name__ == '__main__':
-    reference_img_id_counter = retrieve_similar_features(query_app_id='863550')
+    descriptor_database = np.load(get_descriptor_database_filename())
+    descriptor_img_id = np.load(get_descriptor_img_id_filename())
+
+    query_app_ids = ['620', '364470', '504230', '583950', '646570', '863550', '794600']
+
+    for query_app_id in query_app_ids:
+        reference_app_id_counter = retrieve_similar_features(query_app_id, descriptor_database, descriptor_img_id)
+        print_ranking(query_app_id, reference_app_id_counter)
