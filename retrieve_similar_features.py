9f1a9c98809b82693c8e646a4a0e53e62e93645b
Wok <woctezuma@users.noreply.github.com>
2019-02-08 15:23:49 +0100

    Retrieve similar banners based on features extracted with Keras

--- retrieve_similar_features.py
+++ retrieve_similar_features.py
@@ -3,22 +3,30 @@ from time import time
 
 import cv2 as cv
 import numpy as np
+from keras.preprocessing.image import load_img
 
 from build_feature_index import get_descriptor_database_filename, get_descriptor_img_id_filename
+from build_feature_index import get_label_database_filename, load_keras_model, label_image
 from build_search_index import app_id_to_image_filename, list_app_ids
 from download_steam_banners import get_app_details
 from retrieve_similar_banners import get_store_url
 
 
-def retrieve_similar_features(query_app_id, descriptor_database=None, descriptor_img_id=None):
+def retrieve_similar_features(query_app_id, descriptor_database=None, descriptor_img_id=None,
+                              label_database=None, keras_model=None, target_model_size=None):
     image_filename = app_id_to_image_filename(query_app_id)
-    query_img = cv.imread(image_filename, cv.IMREAD_COLOR)
 
-    # Initiate ORB detector
-    orb = cv.ORB_create()
+    if keras_model is not None:
+        image = load_img(image_filename, target_size=target_model_size)
+        query_des = label_image(image, keras_model)  # runtime: 1 second
+    else:
+        query_img = cv.imread(image_filename, cv.IMREAD_COLOR)
 
-    # find the keypoints and descriptors with ORB
-    _, query_des = orb.detectAndCompute(query_img, None)
+        # Initiate ORB detector
+        orb = cv.ORB_create()
+
+        # find the keypoints and descriptors with ORB
+        _, query_des = orb.detectAndCompute(query_img, None)
 
     # FLANN parameters
     FLANN_INDEX_LSH = 6
@@ -36,31 +44,46 @@ def retrieve_similar_features(query_app_id, descriptor_database=None, descriptor
     if descriptor_img_id is None:
         descriptor_img_id = np.load(get_descriptor_img_id_filename())
 
+    if label_database is None:
+        label_database = np.load(get_label_database_filename())
+
     app_ids = list_app_ids()
 
-    # Remove the features of the query from the database
-    ind, _ = np.where(descriptor_img_id == app_ids.index(query_app_id))
-    trimmed_descriptor_database = descriptor_database.copy()
-    trimmed_descriptor_database[ind] = 0
+    if keras_model is not None:
+        row_no = app_ids.index(query_app_id)
+        trimmed_descriptor_database = label_database.copy()
+        trimmed_descriptor_database[row_no, :] = 0
+
+        num_neighbors = 10
+    else:
+        # Remove the features of the query from the database
+        ind, _ = np.where(descriptor_img_id == app_ids.index(query_app_id))
+        trimmed_descriptor_database = descriptor_database.copy()
+        trimmed_descriptor_database[ind] = 0
+
+        num_neighbors = 2
 
     start = time()
-    matches = flann.knnMatch(query_des, trimmed_descriptor_database, k=2)
+    matches = flann.knnMatch(query_des, trimmed_descriptor_database, k=num_neighbors)
     print('Elapsed time: {:.2f} s'.format(time() - start))
 
-    # store all the good matches as per Lowe's ratio test.
-    good_matches = []
-    for t in matches:
-        try:
-            if t[0].distance < 0.7 * t[1].distance:
-                good_matches.append(t[0])
-        except IndexError:
-            pass
+    if keras_model is not None:
+        reference_app_id_counter = [app_ids[element] for element in matches]
+    else:
+        # store all the good matches as per Lowe's ratio test.
+        good_matches = []
+        for t in matches:
+            try:
+                if t[0].distance < 0.7 * t[1].distance:
+                    good_matches.append(t[0])
+            except IndexError:
+                pass
 
-    good_img_ids = [int(descriptor_img_id[good_match.trainIdx]) for good_match in good_matches]
+        good_img_ids = [int(descriptor_img_id[good_match.trainIdx]) for good_match in good_matches]
 
-    good_app_ids = [app_ids[img_id] for img_id in good_img_ids]
+        good_app_ids = [app_ids[img_id] for img_id in good_img_ids]
 
-    reference_app_id_counter = Counter(good_app_ids)
+        reference_app_id_counter = Counter(good_app_ids)
 
     return reference_app_id_counter
 
@@ -83,9 +106,19 @@ def print_ranking(query_app_id, reference_app_id_counter, num_elements_displayed
 if __name__ == '__main__':
     descriptor_database = np.load(get_descriptor_database_filename())
     descriptor_img_id = np.load(get_descriptor_img_id_filename())
+    label_database = np.load(get_label_database_filename())
 
     query_app_ids = ['620', '364470', '504230', '583950', '646570', '863550', '794600']
 
+    use_keras_features = True
+
+    if use_keras_features:
+        keras_model, target_model_size = load_keras_model(include_top=False)
+    else:
+        keras_model = None
+        target_model_size = None
+
     for query_app_id in query_app_ids:
-        reference_app_id_counter = retrieve_similar_features(query_app_id, descriptor_database, descriptor_img_id)
+        reference_app_id_counter = retrieve_similar_features(query_app_id, descriptor_database, descriptor_img_id,
+                                                             label_database, keras_model, target_model_size)
         print_ranking(query_app_id, reference_app_id_counter)
