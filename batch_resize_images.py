d3923125035a6ce1898d8b2f0811e9c69bbc8990
Wok <woctezuma@users.noreply.github.com>
2019-04-09 00:28:14 +0200

    Possibility to center-crop images

--- batch_resize_images.py
+++ batch_resize_images.py
@@ -3,16 +3,50 @@ from pathlib import Path
 from time import time
 
 import cv2
+import numpy as np
 
 
-def resize_all_images(output_shape=None, display_images=False, verbose_interval=1000):
+def center_crop(img, new_width=None, new_height=None):
+    # Reference: https://stackoverflow.com/a/32385865/
+
+    width = img.shape[1]
+    height = img.shape[0]
+
+    if new_width is None:
+        new_width = min(width, height)
+
+    if new_height is None:
+        new_height = min(width, height)
+
+    left = int(np.ceil((width - new_width) / 2))
+    right = width - int(np.floor((width - new_width) / 2))
+
+    top = int(np.ceil((height - new_height) / 2))
+    bottom = height - int(np.floor((height - new_height) / 2))
+
+    if len(img.shape) == 2:
+        center_cropped_img = img[top:bottom, left:right]
+    else:
+        center_cropped_img = img[top:bottom, left:right, ...]
+
+    return center_cropped_img
+
+
+def resize_all_images(output_shape=None, display_images=False, verbose_interval=1000,
+                      sub_folder=None, apply_center_crop=False):
     if output_shape is None:
         output_shape = [128, 128]
 
     output_folder = 'x'.join([str(i) for i in output_shape]) + '/'
     Path(output_folder).mkdir(exist_ok=True)
 
-    file_paths = glob.glob('data/*.jpg')
+    input_folder = 'data/'
+    if sub_folder is not None:
+        input_folder += sub_folder
+    if not input_folder.endswith('/'):
+        input_folder += '/'
+
+    file_paths = glob.glob(input_folder + '*.jpg')
 
     start = time()
 
@@ -23,6 +57,8 @@ def resize_all_images(output_shape=None, display_images=False, verbose_interval=
         if not Path(output_path).exists():
 
             image = cv2.imread(input_path)
+            if apply_center_crop:
+                image = center_crop(image)
             resized_image = cv2.resize(image, tuple(output_shape))
             cv2.imwrite(output_path, resized_image)
 
